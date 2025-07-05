3b850e6d9462f7f05e2c2e708a55314fcea3fefc
Magnus <magnus@trandokken.com>
2025-07-05 23:08:31 +0200

    restructure

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from screens.analyze import AnalyzeScreen
 from widgets.sensor_card import SensorCard
 from screens.sensor_detail import SensorDetail
 from screens.home import HomeScreen
-from screens.settings import SettingsScreen
-from screens.calibrate_o2 import CalibrateO2Screen
-from screens.wifi_settings import WiFiSettingsScreen
+from screens.settings.settings import SettingsScreen
+from screens.settings.calibrate_o2 import CalibrateO2Screen
+from screens.settings.wifi_settings import WiFiSettingsScreen
 from widgets.menu_card import MenuCard
 from widgets.settings_button import SettingsButton
 # etc.
@@ -43,9 +43,9 @@ class TrimixApp(App):
         Builder.load_file(os.path.join(KV_DIR, 'screens', 'home.kv'))
         Builder.load_file(os.path.join(KV_DIR, 'screens', 'analyze.kv'))
         Builder.load_file(os.path.join(KV_DIR, 'screens', 'sensor_detail.kv'))
-        Builder.load_file(os.path.join(KV_DIR, 'screens', 'settings.kv'))
-        Builder.load_file(os.path.join(KV_DIR, 'screens', 'calibrate_o2.kv'))
-        Builder.load_file(os.path.join(KV_DIR, 'screens', 'wifi_settings.kv'))
+        Builder.load_file(os.path.join(KV_DIR, 'screens', 'settings', 'settings.kv'))
+        Builder.load_file(os.path.join(KV_DIR, 'screens', 'settings', 'calibrate_o2.kv'))
+        Builder.load_file(os.path.join(KV_DIR, 'screens', 'settings', 'wifi_settings.kv'))
         Builder.load_file(os.path.join(KV_DIR, 'app.kv'))
         # 4) Instantiate and return the manager
         return TrimixScreenManager(transition=FadeTransition())
